c5d0f30803a9e460a0467b8b3db3cac839da7431
Ming-X <fmx9978@gmail.com>
2025-12-13 14:44:04 -0800

    Refactor button control for LED brightness adjustment
    Refactored LED control logic to adjust brightness based on button presses. Added print statements for debugging.

--- Tutorial/button_control_LED.py
+++ Tutorial/button_control_LED.py
@@ -11,8 +11,8 @@ buttonA = Pin(11, Pin.IN, Pin.PULL_UP)
 buttonB = Pin(10, Pin.IN, Pin.PULL_UP)
 #end of buttons
 
-color = (0,0,0)
-lu = 0
+color = (8,0,0)
+lu = 8
 rgb_leds.fill(color)
 rgb_leds.write()
 
@@ -21,30 +21,27 @@ def button_pressed(button):
         return True
     else:
         return False
-'''
-while True:
-    if button_pressed(buttonA):
-        color = (4, 0, 0)
-        rgb_leds.fill(color)
-        rgb_leds.write()
-    
-    if button_pressed(buttonB):
-        color = (0, 0, 4)
-        rgb_leds.fill(color)
-        rgb_leds.write()
-'''
+
+print("start")
 while True:
     if button_pressed(buttonA):
         lu-=1
         if lu < 0:
             lu = 0
         color = (lu, 0, 0)
+        print(lu)
         rgb_leds.fill(color)
         rgb_leds.write()
+        sleep(0.1)
     
     if button_pressed(buttonB):
         lu+=1
         if lu > 8:
             lu = 8
+        color = (lu, 0, 0)
+        print(lu)
         rgb_leds.fill(color)
         rgb_leds.write()
+        sleep(0.1)
+        
+    sleep(0.1)
